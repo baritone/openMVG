75714d391dbeef125ee5d800ceaedb02e9af9009
sflotron <s.flotron@foxel.ch>
2014-10-24 11:00:29 +0200

    add data container for rigs BA. #2

--- src/openMVG/bundle_adjustment/problem_data_container.hpp
+++ src/openMVG/bundle_adjustment/problem_data_container.hpp
@@ -133,6 +133,107 @@ class BA_Problem_data_camMotionAndIntrinsic {
 
 };
 
+/// Container for a Bundle Adjustment dataset
+/// Allows to refine rigs extrinsics, cameras shared intrinsics and the structure
+/// Camera can be parametrized by the number of desired values
+///   External parameter => 6: [Rotation(angle,axis), t]
+///   Intrinsic => 1: [Focal]
+template<
+  unsigned char NRigExternalParam = 6,
+  unsigned char NExternalParam = 6,
+  unsigned char NIntrinsicParam = 1>
+class BA_Problem_data_rigMotionAndIntrinsic {
+ public:
+
+  // Number of camera parameters
+  static const unsigned char NRIGEXTERNALPARAM = NRigExternalParam;
+  static const unsigned char NEXTERNALPARAM    = NExternalParam;
+  static const unsigned char NINTRINSICPARAM   = NIntrinsicParam;
+
+  /// Return the number of observed 3D points
+  size_t num_observations()    const { return num_observations_; }
+  /// Return a pointer to observed points [X_0, ... ,X_n]
+  const double* observations() const { return &observations_[0]; }
+
+  /// Return the number of different rigs positions
+  size_t num_rigposition() const { return num_rigs_ ;}
+  /// Return the number of extrinsic groups
+  size_t num_extrinsics()  const { return num_cameras_ ;}
+  /// Return the number of intrinsic groups
+  size_t num_intrinsics()  const { return num_intrinsics_;}
+
+  /// Return a pointer to external rig data
+  double* mutable_rigs_extrinsic() {
+    return &parameters_[0];}
+  /// Return a pointer to external camera data
+  double* mutable_cameras_extrinsic() {
+    return &parameters_[0] + NRigExternalParam * num_rigs_;}
+  /// Return a pointer to intrinsic camera data
+  double* mutable_cameras_intrinsic() {
+    return &parameters_[0]
+      + NRigExternalParam * num_rigs_
+      + NExternalParam * num_cameras_;}
+  /// Return a pointer to 3D points data
+  double* mutable_points()  {
+    return &parameters_[0]
+      + NRigExternalParam * num_rigs_
+      + NExternalParam * num_cameras_
+      + NIntrinsicParam * num_intrinsics_;}
+
+
+  /// Return a pointer to the rig extrinsic that observe the Inth observation
+  double* mutable_rig_extrinsic_for_observation(size_t i) {
+    return mutable_rigs_extrinsic() + rig_index_extrinsic[i] * NRigExternalParam;
+  }
+  /// Return a pointer to the camera extrinsic that observe the Inth observation
+  double* mutable_camera_extrinsic_for_observation(size_t i) {
+    return mutable_cameras_extrinsic() + camera_index_extrinsic[i] * NExternalParam;
+  }
+  /// Return a pointer to the camera intrinsic that observe the Inth observation
+  double* mutable_camera_intrinsic_for_observation(size_t i) {
+    return mutable_cameras_intrinsic() + camera_index_intrinsic[i] * NIntrinsicParam;
+  }
+
+
+  /// Return a pointer to the Inth rig extrinsic parameters
+  double* mutable_rigs_extrinsic(size_t i) {
+    return mutable_rigs_extrinsic() + i * NRigExternalParam;
+  }
+  /// Return a pointer to the Inth intrinsic parameters
+  double* mutable_cameras_intrinsic(size_t i) {
+    return mutable_cameras_intrinsic() + i * NIntrinsicParam;
+  }
+  /// Return a pointer to the Inth extrinsic parameters
+  double* mutable_cameras_extrinsic(size_t i) {
+    return mutable_cameras_extrinsic() + i * NExternalParam;
+  }
+
+
+  /// Return a pointer to the point that observe the Inth observation
+  double* mutable_point_for_observation(size_t i) {
+    return mutable_points() + point_index_[i] * 3;
+  }
+
+  size_t num_rigs_ ;        // # of rigs
+  size_t num_cameras_;      // # of cameras
+  size_t num_intrinsics_;   // # of intrinsic groups
+  size_t num_points_;       // # of 3D points
+  size_t num_observations_; // # of observations
+  // # of parameters: NIntrinsicParam * num_cameras_ + NIntrinsicParam * num_intrinsics_ + 3 * num_points_
+  size_t num_parameters_;
+
+  std::vector<size_t> point_index_;  // re-projection linked to the Inth 2d point
+  std::vector<size_t> rig_index_extrinsic; // re-projection linked to the Inth rig extrinsic
+  std::vector<size_t> camera_index_extrinsic; // re-projection linked to the Inth camera extrinsic
+  std::vector<size_t> camera_index_intrinsic; // re-projection linked to the Inth camera intrinsic
+  std::vector<double> observations_; // 3D points
+
+  // Camera parametrization ([R|t]_0,...,[R|t]_n,[f]_0,...,[f]_n)
+  std::vector<double> parameters_;
+
+};
+
+
 } // namespace bundle_adjustment
 } // namespace openMVG
 
