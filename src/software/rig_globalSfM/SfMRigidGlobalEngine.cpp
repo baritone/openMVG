bd30f35276b1f3bd8a0e7243df054926974cc233
sflotron <s.flotron@foxel.ch>
2015-01-26 15:48:41 +0100

    export rotation and center instead of rotation and translation

--- src/software/rig_globalSfM/SfMRigidGlobalEngine.cpp
+++ src/software/rig_globalSfM/SfMRigidGlobalEngine.cpp
@@ -1362,13 +1362,16 @@ bool GlobalRigidReconstructionEngine::Process()
       const PinholeCamera & cam = iter->second;
       _reconstructorData.map_Camera[iter->first] = BrownPinholeCamera(cam._P);
       _reconstructorData.set_imagedId.insert(iter->first);
-
       _reconstructorData.map_Rig[iter->first] = _map_RigIdPerImageId.at(iter->first);
-      const size_t rigId = _map_RigIdPerImageId.at(iter->first);
       _reconstructorData.set_rigId.insert(rigId);
       _reconstructorData.map_subCamIdperImageId[iter->first] = _map_subCamIdPerImageId.at(iter->first);
-      _reconstructorData.map_posePerRigId[rigId].first  = _map_rig.at(rigId).first;
-      _reconstructorData.map_posePerRigId[rigId].second = _map_rig.at(rigId).second;
+
+      const size_t rigId = _map_RigIdPerImageId.at(iter->first);
+      const Mat3 R       = _map_rig.at(rigId).first;
+      const Mat3 t       = _map_rig.at(rigId).second;
+
+      _reconstructorData.map_posePerRigId[rigId].first  = R;
+      _reconstructorData.map_posePerRigId[rigId].second = -R.transpose() * t;
     }
 
     // Structure
